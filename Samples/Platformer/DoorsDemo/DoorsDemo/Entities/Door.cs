88e1545dfa8ceec0534ccb7959c698e6e1a64d9b
Ryszard Pyka <60892050+RychuP@users.noreply.github.com>
2024-11-19 01:09:56 +0000

    Updated DoorsDemo to NET 6.

--- Samples/Platformer/DoorsDemo/DoorsDemo/Entities/Door.cs
+++ Samples/Platformer/DoorsDemo/DoorsDemo/Entities/Door.cs
@@ -8,41 +8,38 @@ using FlatRedBall.AI.Pathfinding;
 using FlatRedBall.Graphics.Animation;
 using FlatRedBall.Graphics.Particle;
 using FlatRedBall.Math.Geometry;
+using Microsoft.Xna.Framework;
 
 namespace DoorsDemo.Entities
 {
     public partial class Door
     {
         /// <summary>
-        /// Initialization logic which is execute only one time for this Entity (unless the Entity is pooled).
+        /// Initialization logic which is executed only one time for this Entity (unless the Entity is pooled).
         /// This method is called when the Entity is added to managers. Entities which are instantiated but not
         /// added to managers will not have this method called.
         /// </summary>
         private void CustomInitialize()
         {
-
-
+            
         }
-
-        public void Open() => SpriteInstance.CurrentChainName = "Open";
-        public void Close() => SpriteInstance.CurrentChainName = "Closed";
 
         private void CustomActivity()
         {
-
-
+            
         }
 
         private void CustomDestroy()
         {
-
-
+            
         }
 
         private static void CustomLoadStaticContent(string contentManagerName)
         {
-
-
+            
         }
+
+        public void Open() => SpriteInstance.CurrentChainName = "Open";
+        public void Close() => SpriteInstance.CurrentChainName = "Closed";
     }
 }
